eb3182511212affe351bc6d850fe69275bde5a71
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-07 12:06:36 +0300

    add hexlet' solution for html_builder

--- src/solution.js
+++ src/solution.js
@@ -1,42 +1,45 @@
 // BEGIN (write your solution here)
-const helpers = {
-  isArray(entity) {
-    return Array.isArray(entity);
+const propertyActions = [
+  {
+    name: 'body',
+    check: arg => typeof arg === 'string'
   },
-  isObject(entity) {
-    return Object.prototype.toString.call(entity) === '[object Object]';
+  {
+    name: 'children',
+    check: arg => arg instanceof Array
   },
-  isString(entity) {
-    return typeof entity === 'string';
+  {
+    name: 'attributes',
+    check: arg => arg instanceof Object
   }
-};
+];
 
-const buildHtml = tree => {
-  const [tag, ...data] = tree;
-  if (data.length === 0) {
-    return `<${tag}></${tag}>`;
-  }
-  const attrs = data
-    .map(item => {
-      if (helpers.isObject(item)) {
-        return ` ${Object.entries(item)
-          .map(([prop, val]) => `${prop}="${val}"`)
-          .join(' ')}`;
-      }
-      return null;
-    })
-    .filter(v => v);
-  return `<${tag}${attrs}>${data
-    .map(item => {
-      if (helpers.isString(item)) {
-        return `${item}`;
-      }
-      if (helpers.isArray(item)) {
-        return `${item.reduce((acc, el) => `${acc}${buildHtml(el)}`, ``)}`;
-      }
-      return ``;
-    })
-    .join('')}</${tag}>`;
+const getPropertyAction = arg =>
+  propertyActions.find(({ check }) => check(arg));
+
+const buildAttrString = attrs =>
+  Object.keys(attrs)
+    .map(key => ` ${key}="${attrs[key]}"`)
+    .join('');
+
+const buildHtml = data => {
+  const [first, ...rest] = data;
+  const root = {
+    name: first,
+    attributes: {},
+    body: '',
+    children: []
+  };
+  const tag = rest.reduce((acc, arg) => {
+    const { name } = getPropertyAction(arg);
+    return { ...acc, [name]: arg };
+  }, root);
+
+  return [
+    `<${tag.name}${buildAttrString(tag.attributes)}>`,
+    `${tag.body}${tag.children.map(buildHtml).join('')}`,
+    `</${tag.name}>`
+  ].join('');
 };
 
 export default buildHtml;
