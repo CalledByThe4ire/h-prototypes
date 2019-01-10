8d622ab01b7c266a8839d8898416b93063dc974f
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-10 17:04:43 +0300

    add solution for inheritance

--- src/Node.js
+++ src/Node.js
@@ -1,3 +1,18 @@
 // BEGIN (write your solution here)
+export default class {
+  constructor(name, attributes = {}) {
+    this.name = name;
+    this.attributes = attributes;
+  }
 
+  getName() {
+    return this.name;
+  }
+
+  getAttributesAsLine() {
+    return Object.entries(this.attributes)
+      .map(([key, value]) => ` ${key}="${value}"`)
+      .join('');
+  }
+}
 // END
