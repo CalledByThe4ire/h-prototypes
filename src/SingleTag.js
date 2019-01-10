22ce820ea6a6530649896695f5f7034473db3b6e
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-10 15:16:37 +0300

    add hexlet' solution for polymorphism

--- src/SingleTag.js
+++ src/SingleTag.js
@@ -1,3 +1,18 @@
-// BEGIN (write your solution here)
+// BEGIN
+export default class {
+  constructor(name, attributes = {}) {
+    this.name = name;
+    this.attributes = attributes;
+  }
 
+  toString() {
+    return `<${this.name}${this.getAttributesAsLine()}>`;
+  }
+
+  getAttributesAsLine() {
+    return Object.entries(this.attributes)
+      .map(([key, value]) => ` ${key}="${value}"`)
+      .join('');
+  }
+}
 // END
