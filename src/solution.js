5a162953ffdc6e4d34c62fdcc497d9445443043a
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-13 14:59:21 +0300

    add hexlet' solution for js_prototype_extract_nested_values-challenge

--- src/solution.js
+++ src/solution.js
@@ -1,17 +1,11 @@
 /* eslint-disable no-extend-native */
 
 // BEGIN (write your solution here)
-Object.prototype.hash = function hash(str) {
-  function iter(acc, keys) {
-    const [key] = keys;
-    if (keys.length === 1) {
-      return acc[key];
-    }
-    if (acc.hasOwnProperty(key)) {
-      return iter(acc[key], keys.slice(1));
-    }
-    return undefined;
-  }
-  return iter(this, str.split('.'));
+Object.prototype.hash = function hash(path) {
+  const keys = path.split('.');
+  return keys.reduce(
+    (acc, item) => (acc === undefined ? acc : acc[item]),
+    this
+  );
 };
 // END
