69f23cfdad9d9a24a5917a1d3a98a89864b28ade
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-13 14:58:18 +0300

    add solution for js_prototype_extract_nested_values-challenge

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,17 @@
 /* eslint-disable no-extend-native */
 
 // BEGIN (write your solution here)
-
+Object.prototype.hash = function hash(str) {
+  function iter(acc, keys) {
+    const [key] = keys;
+    if (keys.length === 1) {
+      return acc[key];
+    }
+    if (acc.hasOwnProperty(key)) {
+      return iter(acc[key], keys.slice(1));
+    }
+    return undefined;
+  }
+  return iter(this, str.split('.'));
+};
 // END
